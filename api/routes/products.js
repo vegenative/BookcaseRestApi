055a1d71062c75ba6c0b8e26f277672274dc1b58
Paweł Woźniak <WPawel78@gmail.com>
2020-01-16 22:26:10 +0100

    add orders and responses

--- api/routes/products.js
+++ api/routes/products.js
@@ -9,9 +9,36 @@ router.get('/', (req, res, next) => {
 });
 // Post request
 router.post('/', (req, res, next)=>{
-    res.status(200).json({
+    res.status(201).json({
         message: "POST request to /products"
     })
 });
+/////////////productID/////////////////////////////
+//Get
+router.get('/:productId', (req,res,next)=>{
+    const id = req.params.productId;
+    if(id === 'special'){
+        res.status(200).json({
+            message: 'You discovered the special ID',
+            id: id
+        });
+    } else {
+        res.status(200).json({
+            message: 'You have an ID'
+        });
+    }
+});
+//Patch
+router.patch('/:productId', (req,res,next)=>{
+   res.status(200).json({
+       message:"Update product!"
+   });
+});
+// delete
+router.delete('/:productId', (req,res,next)=>{
+    res.status(200).json({
+        message:"deleted product!"
+    });
+ });
 
 module.exports = router;
--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 
 const productRoutes = require('./api/routes/products') //targetujemy 
+const orderRoutes = require('./api/routes/orders')
 
 app.use('/products', productRoutes); //wszystko co ma /products trafi do folderu
+app.use('/orders', orderRoutes);
 // app.use((req, res, next) => {
 //     res.status(200).json({
 //         message: 'It works'
