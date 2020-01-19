d6c766c5998d31252d4c38d83168ba8af5ff8eb8
Paweł Woźniak <WPawel78@gmail.com>
2020-01-19 14:32:50 +0100

    add node.js server

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const productRoutes = require('./api/routes/products') //targetujemy 
 const orderRoutes = require('./api/routes/orders')
 
+app.use(morgan('dev'));
+
 app.use('/products', productRoutes); //wszystko co ma /products trafi do folderu
 app.use('/orders', orderRoutes);
-// app.use((req, res, next) => {
-//     res.status(200).json({
-//         message: 'It works'
-//     });
-// });
+
+////////errors//////////
+
+// nie znaleziono routa
+app.use((req, res, next) => {
+    const error = new Error('Nie znaleziono');
+    error.status = 404;
+    next(error);
+})
+
+//
+app.use((error, req, res, next) => {
+    res.status(err.status || 500);
+    res.json({
+        error:{
+            message: error.message
+        }
+    });
+});
+
 
 module.exports = app;
